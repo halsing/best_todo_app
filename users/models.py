a61b1ff9f0c56a0525862808c6ddc7f09cce0e19
halsing <adrian.zieba2@gmail.com>
2019-08-26 11:07:51 +0200

    first users models tests

--- users/models.py
+++ users/models.py
@@ -2,13 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 
+
 # Create your models here.
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
     image = models.ImageField(
-        default="profile_image/default.jpg" ,
+        default="profile_image/default.jpg",
         upload_to="profile_image",
-        verbose_name="Zdjęcie profilowe"
+        verbose_name="Zdjęcie profilowe",
     )
 
     def __str__(self):
@@ -19,7 +20,7 @@ class Profile(models.Model):
 
         img = Image.open(self.image.path)
 
-        if img.height > 300 or img.width > 300 :
-            output_size = (300,300)
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
             img.thumbnail(output_size)
             img.save(self.image.path)
---
+++ users/tests/models_tests.py
@@ -0,0 +1,48 @@
+from io import BytesIO
+from PIL import Image
+from django.test import TestCase
+from django.core.files.base import File
+from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
+from users.models import Profile
+
+
+# def create_test_image(
+#         name='test.jpg', ext='JPEG', size=(1600, 1500), color=(256, 0, 0)):
+#     file_obj = BytesIO()
+#     image = Image.new("RGB", size=size, color=color)
+#     image.save(file_obj, ext)
+#     file_obj.seek(0)
+#     return File(file_obj, name=name)
+
+
+class TestProfileModels(TestCase):
+
+    def setUp(self):
+        self.user_test = User.objects.create_user(
+            username="Test1", password="asdasdasd12312", email='asda@o2.pl')
+
+    def test_profile_creation(self):
+        user = User.objects.create(
+            username="taskbuster", password="django-tutorial")
+
+        # Check that a Profile instance has been crated
+        self.assertIsInstance(user.profile, Profile)
+
+        # check that it doesn't try to create another profile instace
+        user.save()
+        self.assertIsInstance(user.profile, Profile)
+
+    def test_profile_username(self):
+        self.assertEqual(self.user_test.profile.user.username, "Test1")
+
+    def test_profile_image_default(self):
+        self.assertEqual(
+            self.user_test.profile.image, "profile_image/default.jpg")
+
+    def test_profile_image_size(self):
+        self.assertLessEqual(self.user_test.profile.image.width, 300)
+
+    def test_profile_image_height_size(self):
+        self.assertLessEqual(self.user_test.profile.image.height, 300)
